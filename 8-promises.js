035b4805a1a0bb9f74b5050d9a4aa5cac8756ef8
Wei Chen <weichenw@gmail.com>
2019-04-10 00:03:05 +1000

    Update promise and async await examples

--- 8-promises.js
+++ 8-promises.js
@@ -1,12 +1,46 @@
-const doWorkPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        //resolve('Success')
-        reject('Its no worky')
-    }, 2000)
-})
+// const doWorkPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         //resolve('Success')
+//         reject('Its no worky')
+//     }, 2000)
+// })
 
-doWorkPromise.then((result) => {
-    console.log('123', result)
-}).catch((error) => {
-    console.log('Error', error)
-}) 
+// doWorkPromise.then((result) => {
+//     console.log('123', result)
+// }).catch((error) => {
+//     console.log('Error', error)
+// }) 
+
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+//Nested -> No good
+// add(1, 2).then((sum) => {
+//     console.log(sum)
+
+//     add(sum, 1).then((sum2) => {
+//         console.log(sum2)
+//     }).catch(() => {
+//         console.log(e)
+//     })
+
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+add(1, 2).then((sum1) => {
+    console.log(sum1)
+    return add(sum1, 1)
+}).then((sum2) => {
+    console.log(sum2)
+    return add(sum2, 2)
+}).then((sum3) => {
+    console.log(sum3)
+}).catch((e) => {
+    console.log(e)
+})
---
+++ 9-async-await.js
@@ -0,0 +1,20 @@
+const add = (a, b) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(a + b)
+        }, 2000)
+    })
+}
+
+const doWork = async () => {
+    const sum1 = await add(1, 1)
+    const sum2 = await add(sum1, 1)
+    const sum3 = await add(sum2, 1)
+    return sum3
+}
+
+doWork().then((result) => {
+    console.log('result', result)
+}).catch((e) => {
+    console.log('e', e)
+})
